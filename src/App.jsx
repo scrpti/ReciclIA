ec39bdaee8d9351eddc5b35be2d4c175aadf612f
allitojimenezuma <allitojim193@uma.es>
2024-03-15 12:07:55 +0100

    instalo

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <p className=' text-xl text-blue-500'>Hola</p>
+
+    </>
+  )
+}
+
+export default App
